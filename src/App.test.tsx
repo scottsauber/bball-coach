f1f90c27cb297d6c1506020827870abd902554d1
Scott Sauber <stsauber@gmail.com>
2022-02-06 20:55:55 -0600

    failing test for adding a player

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,17 @@
 import React from "react";
 import { render, screen } from "@testing-library/react";
 import App from "./App";
+import userEvent from "@testing-library/user-event";
 
-test("renders learn react link", () => {
+test("entering player name and clicking add button should add new players", () => {
     render(<App />);
-    const linkElement = screen.getByText(/learn react/i);
-    expect(linkElement).toBeInTheDocument();
+    const nameTextbox = screen.getByLabelText("Player Name");
+    const playerName = "Player 1";
+    userEvent.type(nameTextbox, playerName);
+    const addButton = screen.getByRole("button", { name: /Add Player/i });
+
+    userEvent.click(addButton);
+
+    const playerOne = screen.getByText(playerName);
+    expect(playerOne).toBeInTheDocument();
 });
