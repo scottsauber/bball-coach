4376226252558c67b28acc3a3002e97b7bbccd6f
Scott Sauber <stsauber@gmail.com>
2022-02-06 21:12:21 -0600

    refactor tests

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -4,26 +4,28 @@ import App from "./App";
 import userEvent from "@testing-library/user-event";
 
 test("entering player name and clicking add button should add new players", () => {
-    render(<App />);
-    const nameTextbox = screen.getByLabelText("Player Name");
     const playerName = "Player 1";
-    userEvent.type(nameTextbox, playerName);
-    const addButton = screen.getByRole("button", { name: /Add Player/i });
 
-    userEvent.click(addButton);
+    addPlayerFlow(playerName);
 
     const playerOne = screen.getByText(playerName);
     expect(playerOne).toBeInTheDocument();
 });
 
 test("clicking add button should clear player name textbox", () => {
-    render(<App />);
-    const nameTextbox = screen.getByLabelText("Player Name") as HTMLInputElement;
     const playerName = "Player 1";
+
+    addPlayerFlow(playerName);
+
+    const nameTextbox = screen.getByLabelText("Player Name") as HTMLInputElement;
+    expect(nameTextbox.value).toBe("");
+});
+
+const addPlayerFlow = (playerName: string) => {
+    render(<App />);
+    const nameTextbox = screen.getByLabelText("Player Name");
     userEvent.type(nameTextbox, playerName);
     const addButton = screen.getByRole("button", { name: /Add Player/i });
 
     userEvent.click(addButton);
-
-    expect(nameTextbox.value).toBe("");
-});
+};
