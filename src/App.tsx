ff68aa4f89bd4fc5536440c92c25023c863166ac
Scott Sauber <stsauber@gmail.com>
2022-02-06 21:04:47 -0600

    make add player test pass

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
+    const [playerName, setPlayerName] = useState("");
+    const [players, setPlayers] = useState<string[]>([]);
+
+    const addPlayer = () => {
+        setPlayers([...players, playerName]);
+    };
+
+    const updatePlayer = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setPlayerName(event.target.value);
+    };
+
     return (
         <div className="App">
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                <p>
-                    Edit <code>src/App.tsx</code> and save to reload.
-                </p>
-                <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Learn React
-                </a>
-            </header>
+            <label htmlFor="playerName">Player Name</label>
+            <input id="playerName" onChange={updatePlayer} value={playerName} />
+            <br />
+            <button onClick={addPlayer}>Add Player</button>
+            <br />
+            <ul>
+                {players.map((player) => (
+                    <li key={player}>{player}</li>
+                ))}
+            </ul>
         </div>
     );
 }
